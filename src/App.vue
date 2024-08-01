99dcca19ef4237501e1da6b965c0317fbf11e7fc
solomeowl <j19940430@gmail.com>
2024-08-01 20:57:39 +0800

    Configure GitHub Pages deployment

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div id="app">
-    <!-- 您的應用內容 -->
+    <!-- 其他的應用內容 -->
+    <TelegramButton />
   </div>
 </template>
 
 <script>
 import { onMounted } from 'vue'
 import { initTelegramWebApp } from './telegramWebApp'
+import TelegramButton from './components/TelegramButton.vue'
 
 export default {
   name: 'App',
+  components: {
+    TelegramButton
+  },
   setup() {
     onMounted(() => {
       initTelegramWebApp()
--- src/telegramWebApp.js
+++ src/telegramWebApp.js
@@ -7,5 +7,3 @@ export const initTelegramWebApp = () => {
 export const closeTelegramWebApp = () => {
   WebApp.close()
 }
-
-// 添加其他您可能需要的 TWA 功能
