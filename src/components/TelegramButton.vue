d1efc5f2be995d0861061aa3ffb77f29fbf91790
solomeowl <j19940430@gmail.com>
2024-08-01 20:54:36 +0800

    Add TelegramButton component

---
+++ src/components/TelegramButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <button @click="closeApp">Close TWA</button>
+</template>
+
+<script>
+import { closeTelegramWebApp } from '../telegramWebApp'
+
+export default {
+    name: 'TelegramButton',
+    methods: {
+        closeApp() {
+            closeTelegramWebApp()
+        }
+    }
+}
+</script>
+
+<style scoped>
+button {
+    padding: 10px 20px;
+    background-color: #54A9EB;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+</style>
