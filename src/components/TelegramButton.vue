a37acc25757b97f5cfbbc8c24fe0afc618a8b36f
solomeowl <j19940430@gmail.com>
2024-08-02 00:53:50 +0800

    connect wallet test9

--- src/components/TelegramButton.vue
+++ src/components/TelegramButton.vue
@@ -2,23 +2,35 @@
     <div class="telegram-buttons">
         <button @click="closeApp">Close TWA</button>
         <button @click="expandApp">Expand TWA</button>
-        <button @click="connectWallet">Connect Wallet</button>
-        <p v-if="walletAddress">Wallet Address: {{ walletAddress }}</p>
+        <button v-if="!walletAddress" @click="connectWallet">Connect Wallet</button>
+        <button v-else @click="disconnectWallet">Disconnect Wallet</button>
+        <button v-if="walletAddress" @click="signMessage">Sign Message</button>
+        <p v-if="walletAddress">Wallet Address: {{ formattedAddress }}</p>
+        <p v-if="signature">Signature: {{ signature }}</p>
         <p v-if="error">{{ error }}</p>
     </div>
 </template>
 
 <script>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import { closeTelegramWebApp, expandTelegramWebApp } from '../telegramWebApp'
 import TonConnect from '@tonconnect/sdk'
+import { Address } from '@ton/core'
 
 export default {
     name: 'TelegramButton',
     setup() {
         const walletAddress = ref('')
         const error = ref('')
+        const signature = ref('')
         let connector
+
+        const formattedAddress = computed(() => {
+            if (walletAddress.value) {
+                return Address.parseRaw(walletAddress.value).toString()
+            }
+            return ''
+        })
 
         onMounted(() => {
             console.log('TelegramButton mounted')
@@ -32,6 +44,7 @@ export default {
                     console.log('Wallet connected:', walletAddress.value)
                 } else {
                     walletAddress.value = ''
+                    signature.value = ''
                     console.log('Wallet disconnected')
                 }
             })
@@ -57,18 +70,44 @@ export default {
                     bridgeUrl: 'https://bridge.tonapi.io/bridge'
                 }
 
-                // 生成連接 URL
                 const url = await connector.connect(walletConnectionSource)
 
-                // 在 Telegram WebApp 中打開 URL
                 if (window.Telegram && window.Telegram.WebApp) {
                     window.Telegram.WebApp.openLink(url)
                 } else {
-                    // 如果不在 Telegram WebApp 環境中，就在新窗口中打開
                     window.open(url, '_blank')
                 }
             } catch (e) {
                 console.error('Failed to connect wallet:', e)
+                error.value = e.message
+            }
+        }
+
+        const disconnectWallet = async () => {
+            console.log('Disconnecting wallet')
+            try {
+                await connector.disconnect()
+                walletAddress.value = ''
+                signature.value = ''
+            } catch (e) {
+                console.error('Failed to disconnect wallet:', e)
+                error.value = e.message
+            }
+        }
+
+        const signMessage = async () => {
+            console.log('Signing message')
+            try {
+                const messageToSign = 'Hello, TON!'
+                const signatureResult = await connector.sendRequest({
+                    method: 'ton_sign',
+                    params: {
+                        message: btoa(messageToSign)
+                    }
+                })
+                signature.value = signatureResult
+            } catch (e) {
+                console.error('Failed to sign message:', e)
                 error.value = e.message
             }
         }
@@ -83,10 +122,14 @@ export default {
 
         return {
             walletAddress,
+            formattedAddress,
             error,
+            signature,
             closeApp,
             expandApp,
-            connectWallet
+            connectWallet,
+            disconnectWallet,
+            signMessage
         }
     }
 }
