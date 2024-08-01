c56d8bc94c3dbc0b1c5ee0592476f36b11f51a25
solomeowl <j19940430@gmail.com>
2024-08-02 00:19:05 +0800

    connect wallet test5

--- src/telegramWebApp.js
+++ src/telegramWebApp.js
@@ -1,13 +1,22 @@
-import * as TWA from '@twa-dev/sdk'
+const TWA = window.Telegram.WebApp;
 
 export const initTelegramWebApp = () => {
-  TWA.ready()
+  TWA.ready();
 }
 
 export const closeTelegramWebApp = () => {
-  TWA.close()
+  TWA.close();
 }
 
 export const expandTelegramWebApp = () => {
-  TWA.expand()
+  TWA.expand();
+}
+
+// 你可以根據需要添加更多的函數
+export const isExpanded = () => {
+  return TWA.isExpanded;
+}
+
+export const sendData = (data) => {
+  TWA.sendData(data);
 }
